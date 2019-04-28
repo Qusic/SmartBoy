f267b33974f4eceb35d89b4f3e30bc186edea9e1
Qusic <qusicx@gmail.com>
2019-04-28 17:29:59 +0800

    cleanup and migrating to k8s deployment

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-container=$(docker-compose ps -q smartboy)
-env=$(docker inspect --format '{{range .Config.Env}}{{println .}}{{end}}' $container | grep '^\(DEBUG\|LOCAL\|TOKEN\)=')
-export $env
-docker-compose down
-docker-compose up -d --build
